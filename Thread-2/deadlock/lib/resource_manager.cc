bd260fe0e76b2f0c851a00ecbe768c141aad9927
CaulCaul <1359167895@qq.com>
2021-11-19 14:01:17 +0800

    Complete Thread-2 deadlock

--- Thread-2/deadlock/lib/resource_manager.cc
+++ Thread-2/deadlock/lib/resource_manager.cc
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <mutex>
 #include <thread>
 #include <chrono>
@@ -7,42 +8,147 @@
 namespace proj2 {
 
 int ResourceManager::request(RESOURCE r, int amount) {
-    if (amount <= 0)  return 1;
 
-    std::unique_lock<std::mutex> lk(this->resource_mutex[r]);
+    //改成总共只需要一个锁！
+
+
+    std::thread::id this_id = std::this_thread::get_id();
+    printf("%7d request %d : %d\n", (*(uint32_t*)&this_id), r, amount);
+
+    if (amount <= 0)  {
+        printf("%7d request with WRONG parameter.\n",(*(uint32_t*)&this_id));
+        return 1;
+    }
+    
+    // std::unique_lock<std::mutex> lk(this->resource_mutex[r]);
+    std::unique_lock<std::mutex> lk(data_lock);
     while (true) {
-        if (this->resource_cv[r].wait_for(
-            lk, std::chrono::milliseconds(100),
-            [this, r, amount] { return this->resource_amount[r] >= amount; }
+        if (this->resource_cv[r].wait_for( lk,  std::chrono::milliseconds(100), 
+            // [this, r, amount] { return this->resource_amount[r] >= amount; }
+            [this, r, amount]{return this->check_security(r, amount);}
         )) {
             break;
         } else {
             auto this_id = std::this_thread::get_id();
-            /* HINT: If you choose to detect the deadlock and recover,
-                     implement your code here to kill and restart threads.
-                     Note that you should release this thread's resources
-                     properly.
-             */
+            /* HINT: If you choose to detect the deadlock and recover, implement your code here to kill and restart threads. Note that you should release this thread's resources properly. */
             if (tmgr->is_killed(this_id)) {
                 return -1;
             }
         }
     }
+    printf("%7d get %d : %d\n", (*(uint32_t*)&this_id), r, amount);
     this->resource_amount[r] -= amount;
-    this->resource_mutex[r].unlock();
+    this->allocation[this_id][r] += amount;
+    // this->available[r] -= amount;
+    // this->resource_mutex[r].unlock();
+    this->data_lock.unlock();
     return 0;
 }
 
+
 void ResourceManager::release(RESOURCE r, int amount) {
+    std::thread::id this_id = std::this_thread::get_id();
+    printf("%7d release %d : %d\n", (*(uint32_t*)&this_id), r, amount);
     if (amount <= 0)  return;
-    std::unique_lock<std::mutex> lk(this->resource_mutex[r]);
+    // std::unique_lock<std::mutex> lk(this->resource_mutex[r]);
+    std::unique_lock<std::mutex> lk(data_lock);
     this->resource_amount[r] += amount;
+    this->allocation[this_id][r] -= amount;
     this->resource_cv[r].notify_all();
 }
 
+bool ResourceManager::check_security(RESOURCE r, int amount) {
+
+    std::map<RESOURCE, int> available;
+    available = resource_amount;
+
+    std::thread::id this_id = std::this_thread::get_id();
+    printf("%7d check %d : %d (now available %d %d %d %d)\n", (*(uint32_t*)&this_id), r, amount, available[GPU], available[MEMORY], available[DISK], available[NETWORK]);
+    // for (int n = 0; n < threadList.size(); n++) {
+    //     printf("%7d check (cur_alloc %d : %d %d %d %d)\n", (*(uint32_t*)&this_id), (*(uint32_t*)&threadList[n]), allocation[threadList[n]][GPU], allocation[threadList[n]][MEMORY], allocation[threadList[n]][DISK], allocation[threadList[n]][NETWORK]);
+    // }
+
+    if (available[r] < amount) {
+        // printf("FAILED more than available\n");
+        return false;
+    }
+    if (allocation[this_id][r] + amount > max[this_id][r]) {
+        // printf("FAILED more than max\n");
+        return false;
+    }
+    // printf("\n");
+
+    available[r] -= amount;
+    allocation[this_id][r] += amount;
+
+
+    for (int i = 0; i < threadList.size(); i++)
+        unfinished[threadList[i]] = true;
+
+    bool done = false;
+    while(!done){
+        done = true;
+        for (int n = 0; n < threadList.size(); n++) {
+            if (!unfinished[threadList[n]]) continue;
+            bool is_available = true;
+            for (int i = 0; i < 4; i++) {
+                // printf("%7d check (%d,%d) : max:%d, alloc:%d, avail:%d\n", (*(uint32_t*)&this_id), (*(uint32_t*)&threadList[n]), i, max[threadList[n]][i], allocation[threadList[n]][i], available[(proj2::RESOURCE)i]);
+                if (max[threadList[n]][i] - allocation[threadList[n]][i] > available[(proj2::RESOURCE)i]) {
+                    is_available = false;
+                    break;
+                }
+            }
+            // printf("%7d check %d : %s\n", (*(uint32_t*)&this_id), (*(uint32_t*)&threadList[n]), is_available?"True":"False");
+            if (is_available) {
+                done = false;
+                for (int i = 0; i < 4; i++) {
+                    available[(proj2::RESOURCE)i] += allocation[threadList[n]][i]; // 没有深拷贝
+                }
+                unfinished[threadList[n]] = false;
+            }
+        }
+    }
+
+    available[r] += amount;
+    allocation[this_id][r] -= amount;
+
+    for (int n = 0; n < threadList.size(); n++) {
+        if (unfinished[threadList[n]]) {
+            // printf("%7d check FAILED\n", (*(uint32_t*)&this_id));
+            return false;
+        }
+    }
+
+    printf("%7d check PASS\n", (*(uint32_t*)&this_id));
+    return true;
+}
+
+
+
 void ResourceManager::budget_claim(std::map<RESOURCE, int> budget) {
     // This function is called when some workload starts.
-    // The workload will eventually consume all resources it claims
+    // The workload will eventually consume all resources it claimss
+    std::thread::id this_id = std::this_thread::get_id();
+
+    this->total_thread++;
+    debug_thread_id[this_id] = total_thread;
+    printf("%7d Total thread: %d\n",(*(uint32_t*)&this_id), this->total_thread);
+
+    this->threadList.push_back(this_id);
+
+    this->max[this_id] = new int [4];
+    this->allocation[this_id] = new int [4];
+
+    for (int i = 0; i < 4; i++) {
+        this->max[this_id][i] = 0;
+        this->allocation[this_id][i] = 0;
+    }
+
+    std::map<RESOURCE, int>::iterator iter;
+    for (iter = budget.begin(); iter != budget.end(); iter++) {
+        this->max[this_id][iter->first] = iter->second;
+        printf("%7d claim %d : %d\n", (*(uint32_t*)&this_id), iter->first, iter->second);
+    }
 }
 
 } // namespace: proj2
--- Thread-2/deadlock/lib/resource_manager.h
+++ Thread-2/deadlock/lib/resource_manager.h
@@ -2,6 +2,7 @@
 #define DEADLOCK_LIB_RESOURCE_MANAGER_H_
 
 #include <map>
+#include <vector>
 #include <mutex>
 #include <thread>
 #include <condition_variable>
@@ -19,15 +20,32 @@ enum RESOURCE {
 class ResourceManager {
 public:
     ResourceManager(ThreadManager *t, std::map<RESOURCE, int> init_count): \
-        resource_amount(init_count), tmgr(t) {}
+        resource_amount(init_count), tmgr(t), total_thread(0) {
+            std::map<RESOURCE, int>::iterator iter;
+            for (iter = resource_amount.begin(); iter != resource_amount.end(); iter++)
+                printf("Resource %d: %d\n", iter->first, iter->second);
+    }
     void budget_claim(std::map<RESOURCE, int> budget);
     int request(RESOURCE, int amount);
     void release(RESOURCE, int amount);
 private:
+    bool check_security(RESOURCE, int amount);
+
+    std::vector<std::thread::id> threadList;
+    std::map<std::thread::id, bool> unfinished;
     std::map<RESOURCE, int> resource_amount;
     std::map<RESOURCE, std::mutex> resource_mutex;
     std::map<RESOURCE, std::condition_variable> resource_cv;
     ThreadManager *tmgr;
+
+    std::mutex data_lock;
+    std::map<std::thread::id, int*> max;
+    // std::map<RESOURCE, int> available;
+    std::map<std::thread::id, int*> allocation;
+
+    std::map<std::thread::id, int> debug_thread_id;
+
+    int total_thread;
 };
 
 }  // namespce: proj2
